b81428f38b32da2ed62100cd789787270a9c0a3c
Douglas Storz <douglasstorz@gmail.com>
2022-05-30 11:05:56 -0400

    Now can gray and blur at once

--- blur.py
+++ blur.py
@@ -2,7 +2,7 @@ import cv2
 import sys
 
 
-def blur_display(infile, nogui=False, blur_degree=7):
+def blur_display(infile, nogui=False, blur_degree=7, stack_operations=False):
     # The first argument is the image
     image = cv2.imread(infile)
 
@@ -10,7 +10,10 @@ def blur_display(infile, nogui=False, blur_degree=7):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # blur it
-    blurred_image = cv2.GaussianBlur(image, (blur_degree, blur_degree), 0)
+    if stack_operations:
+        blurred_image = cv2.GaussianBlur(gray_image, (blur_degree, blur_degree), 0)
+    else:
+        blurred_image = cv2.GaussianBlur(image, (blur_degree, blur_degree), 0)
 
     if nogui:
         cv2.imwrite("test_blurred.png", blurred_image)
@@ -36,7 +39,12 @@ if __name__ == "__main__":
         default=7,
         help="Degree of blur. Must be odd number.",
     )
+    parser.add_argument(
+        "-s",
+        action="store_true",
+        help="Do both operations (output a gray and blurred image)",
+    )
     args = parser.parse_args()
 
     assert args.b % 2 == 1
-    blur_display(args.input_image, args.f, args.b)
+    blur_display(args.input_image, args.f, args.b, args.s)
